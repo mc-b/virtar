de1f63542adc0770f85987c33cb46103ba6a9b1c
mc-b <marcel.bernet@ch-open.ch>
2022-05-28 14:08:04 +0200

    Serverless mit K-Native

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,12 @@
 #
 #	Installationsscript virtar - Serverless
 
+##########################
+# Serverless (Knative) - Installation erfolgt in Juypter
+# kubectl apply --filename https://github.com/knative/serving/releases/download/knative-v1.4.0/serving-crds.yaml
+# kubectl apply --filename https://github.com/knative/serving/releases/download/knative-v1.4.0/serving-core.yaml
+
+# kubectl apply --filename https://github.com/knative/net-istio/releases/download/v0.15.0/release.yaml
+wget https://storage.googleapis.com/knative-nightly/client/latest/kn-linux-amd64 -O kn
+chmod 755 kn
+sudo mv kn /usr/local/bin/
